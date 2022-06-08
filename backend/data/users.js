13ab79543f4bd890507cef58840c99f8a74da192
JohelPires <johelpires@gmail.com>
2022-06-08 10:25:14 -0400

    temporary dummy users db

---
+++ backend/data/users.js
@@ -0,0 +1,22 @@
+import bcryptjs from "bcryptjs"
+
+const users = [
+    {
+        nome: 'Admin',
+        email: 'admin@example.com',
+        password: bcrypt.hashSync('123456', 10),
+        isAdmin: true,
+    },
+    {
+        nome: 'Johel Pires',
+        email: 'johelpires@gmail.com',
+        password: bcrypt.hashSync('123456', 10),
+    },
+    {
+        nome: 'Joe Satriani',
+        email: 'satriani@example.com',
+        password: bcrypt.hashSync('123456', 10),
+    },
+]
+
+export default users
