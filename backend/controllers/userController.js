b11326efb419bd1c1f8c48ca06a57ebb25b47e72
JohelPires <johelpires@gmail.com>
2022-06-13 16:19:58 -0400

    added generateToken via jwt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import expressAsyncHandler from 'express-async-handler'
 import User from '../models/userModel.js'
+import generateToken from '../utils/generateToken.js'
 
 // @desc    Auth user & get token
 // @route   POST /api/users/login
@@ -16,7 +17,7 @@ const authUser = expressAsyncHandler(async (req, res) => {
       nome: user.nome,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: null,
+      token: generateToken(user._id),
     })
   } else {
     res.status(401)
